9c97609baee5ea6f85a6296d74c7d7ace7a37339
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-13 12:42:47 -0300

    Criação do componente Navbar

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {
+    Container,
+    Icon, Input,
+    ImageIcon,
+    Search,
+    RightArea,
+    Text
+} from './styles';
+
+const Navbar = () => {
+    return (
+        <Container>
+            <Text>Bom dia, Diego</Text>
+            <RightArea>
+                <Search>
+                    <Icon className='bx bx-search'></Icon>
+                    <Input type='text' placeholder='Digite aqui...' />
+                </Search>
+                <ImageIcon 
+                src='https://avatars.githubusercontent.com/u/50786415?s=400&u=245c935a0c4567bc224f960be3efa86fcd8df10a&v=4' 
+                alt='Perfil'/>
+                <Icon style={{ fontSize: '2.2rem' }} className='bx bx-bell'></Icon>
+            </RightArea>
+        </Container>
+    )
+}
+
+export default Navbar;
