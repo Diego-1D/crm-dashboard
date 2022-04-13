cc8049d416a8885d062f28f3b3bd8e8f22ebbc7e
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-13 12:45:15 -0300

    Estiização da pagina Dashboard

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.section`
+
+`;
+
+export const Title = styled.h2`
+
+`;
+
+export const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+`;
