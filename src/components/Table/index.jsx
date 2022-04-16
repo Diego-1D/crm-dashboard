021f4458a61f11d82432c4257e7d10df0288ee8c
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-16 11:57:54 -0300

    Criação do componente Table

---
+++ src/components/Table/index.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {TableArea, Title, Body} from './styles';
+
+const Table = () => {
+  return (
+    <TableArea>
+      <Title>Top Customers</Title>
+      <Body>
+        <tr>
+          <td>Diego Fernandes</td>
+          <td>490</td>
+          <td>$15,870</td>
+        </tr>
+        <tr>
+          <td>Diego Fernandes</td>
+          <td>490</td>
+          <td>$15,870</td>
+        </tr>
+        <tr>
+          <td>Diego Fernandes</td>
+          <td>490</td>
+          <td>$15,870</td>
+        </tr>
+        <tr>
+          <td>Diego Fernandes</td>
+          <td>490</td>
+          <td>$15,870</td>
+        </tr>
+        <tr>
+          <td>Diego Fernandes</td>
+          <td>490</td>
+          <td>$15,870</td>
+        </tr>
+        <tr>
+          <td>Diego Fernandes</td>
+          <td>490</td>
+          <td>$15,870</td>
+        </tr>
+      </Body>
+    </TableArea>
+  )
+}
+
+export default Table;
