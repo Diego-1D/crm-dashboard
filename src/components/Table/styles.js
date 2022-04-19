c7a86554c633208ffb6b5a31b799b036f99a028f
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-18 23:33:42 -0300

    Estilização do componente Table

--- src/components/Table/styles.js
+++ src/components/Table/styles.js
@@ -1,13 +1,32 @@
 import styled from "styled-components";
 
 export const TableArea = styled.table`
-    width: 300px;
+    width: 500px;
     background: #FFF;
     box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
     border-radius: 10px;
     padding: 10px 20px;
+    margin-top: 30px;
+    border-spacing: 0;
 `;
 
-export const Title = styled.h1``;
+export const Title = styled.h1`
+    font-size: 1.4rem;
+`;
 
-export const Body = styled.tbody``;
+export const Body = styled.tbody`
+`;
+
+export const Line = styled.tr`
+   display: flex;
+   justify-content: space-between;
+   padding: 10px;
+   border-radius: 2px;
+      
+    &:hover{
+        background-color: pink;
+    }
+`;
+
+export const Column = styled.td`
+`;
