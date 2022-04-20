3b21bd22532e7545cb10b212d34c4c93038b6a16
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-20 10:37:45 -0300

    Estilização do componente TableGrafic

---
+++ src/components/TableGrafic/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Table = styled.table`
+    background: #FFF;
+    box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+    border-radius: 10px;
+    padding: 15px;
+    border-spacing: 0;
+    margin-bottom: 30px;
+`;
+
+export const Body = styled.tbody`
+`;
