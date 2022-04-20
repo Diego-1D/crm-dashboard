40246500f0ad3ae02a88ffe1e5c7e7ec0bb05427
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-20 10:38:48 -0300

    Importação do componente TableGrafic

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import DashboardCard from '../../components/DashboardCard';
 import Table from '../../components/Table';
+import TableGrafic from '../../components/TableGrafic';
 import TableOrders from '../../components/TableOrders';
 import {
     Container,
     Title,
+    Content,
+    Wrapper
 } from './styles';
 
 const Dashboard = () => {
@@ -12,8 +15,16 @@ const Dashboard = () => {
         <Container>
             <Title>Dashboard</Title>
             <DashboardCard />
-            <Title>Principais clientes</Title>
-            <Table />
+            <Content>
+                <Wrapper>
+                    <Title>Principais clientes</Title>
+                    <Table />
+                </Wrapper>
+                <Wrapper>
+                    <Title>Totais de vendas</Title>
+                    <TableGrafic />
+                </Wrapper>
+            </Content>
             <Title>Últimos pedidos</Title>
             <TableOrders />
         </Container>
