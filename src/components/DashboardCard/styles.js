62fa2895b4904cca57c0bd91ab29ecb39a0fc654
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-19 22:12:17 -0300

    Alterações

--- src/components/DashboardCard/styles.js
+++ src/components/DashboardCard/styles.js
@@ -1,5 +1,12 @@
 import styled from "styled-components";
 
+export const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+`;
+
 export const Card = styled.div`
     width: 200px;
     height: 130px;
@@ -17,11 +24,11 @@ export const Icon = styled.i`
     color: #7A90A3;
 `;
 
-export const Name = styled.h3`
+export const Title = styled.h3`
     font-size: 1rem;
 `;
 
-export const Value = styled.p`
+export const Count = styled.p`
     font-size: 1.2rem;
     font-weight: bold;
     color: #7A90A3;
