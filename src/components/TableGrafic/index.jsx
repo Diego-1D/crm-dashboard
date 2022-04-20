b7bfc6075ab8665694f7c50dd67b617d55a1249f
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-20 10:36:59 -0300

    Criação do componente TableGrafic

---
+++ src/components/TableGrafic/index.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Table, Body } from './styles';
+import Chart from 'react-apexcharts';
+
+const TableGrafic = () => {
+
+  const chartOptions = {
+    options: {
+      chart: {
+        id: "basic-bar",
+      },
+      xaxis: {
+        categories: ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago"]
+      },
+    },
+    series: [
+      {
+        name: "series",
+        data: [30, 40, 45, 50, 49, 60, 87, 79]
+      }
+    ],
+  }
+
+  return (
+    <Table>
+      <Body>
+        <Chart
+          options={chartOptions.options}
+          series={chartOptions.series}
+          type="bar"
+          width="420"
+          height="270"
+        />
+      </Body>
+    </Table>
+  )
+}
+
+export default TableGrafic;
