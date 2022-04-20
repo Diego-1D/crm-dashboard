3109fab1c6a64d8f80a7b84dfe140f327d4078d1
Diego-1D <50786415+Diego-1D@users.noreply.github.com>
2022-04-19 22:14:55 -0300

    Paginação da tabela clientes

--- src/pages/Customers/index.jsx
+++ src/pages/Customers/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Body,
   Container,
@@ -6,49 +6,83 @@ import {
   Column,
   Line,
   Title,
-  Head
+  Head,
+  HeadTitle,
+  Pagination,
+  PaginationItem
 } from './styles';
 
 import CustomersList from '../../data/customers-list.json';
 
 const Customers = () => {
 
-  const head = [
-    'Id',
-    'Nome',
-    'E-mail',
-    'Telefone',
-    'Total',
-    'Total II',
-    'Localização',
-  ]
+  let limit = '10';
+  const initDataShow = CustomersList.slice(0, Number(limit));
+
+  const [dataShow, setDataShow] = useState(initDataShow);
+  const [nextPage, setNextPage] = useState(0);
+
+  let pages = 1;
+  let range = [];
+
+  if (limit !== undefined) {
+    let page = Math.floor(CustomersList.length / Number(limit));
+    pages = CustomersList.length % Number(limit) === 0 ? page : page + 1;
+    range = [...Array(pages).keys()];
+  }
+
+  const selectPage = page => {
+    const start = Number(limit) * page;
+    const end = start + Number(limit);
+
+    setDataShow(CustomersList.slice(start, end));
+
+    setNextPage(page);
+  }
+
   return (
     <Container>
       <Title>Clientes</Title>
       <Table>
         <Head>
-          {head.map((index, key) => (
-            <Line key={key}>
-              <Column>{index}</Column>
-            </Line>
-          ))
-          }
+          <HeadTitle>Id</HeadTitle>
+          <HeadTitle>Nome</HeadTitle>
+          <HeadTitle>E-mail</HeadTitle>
+          <HeadTitle>Telefone</HeadTitle>
+          <HeadTitle style={{ textAlign: 'center' }}>Total de pedidos</HeadTitle>
+          <HeadTitle style={{ textAlign: 'center' }}>Total gastos</HeadTitle>
+          <HeadTitle style={{ textAlign: 'center' }}>Local</HeadTitle>
         </Head>
         <Body>
-          {CustomersList.map((index, key) => (
+          {dataShow.map((index, key) => (
             <Line key={key}>
               <Column>{index.id}</Column>
               <Column>{index.name}</Column>
               <Column>{index.email}</Column>
               <Column>{index.phone}</Column>
-              <Column>{index.total_orders}</Column>
-              <Column>{index.total_spend}</Column>
-              <Column>{index.location}</Column>
+              <Column style={{ textAlign: 'center' }}>{index.total_orders}</Column>
+              <Column style={{ textAlign: 'center' }}>{index.total_spend}</Column>
+              <Column style={{ textAlign: 'center' }}>{index.location}</Column>
             </Line>
           ))
           }
         </Body>
 
+        {
+          pages > 1 ? (
+            <Pagination>
+              {
+                range.map((item, key) => (
+                  <PaginationItem key={key}
+                    onClick={() => selectPage(key)}>
+                    {item + 1}
+                  </PaginationItem>
+                ))
+              }
+
+            </Pagination>
+          ) : null
+        }
       </Table>
     </Container>
   )
