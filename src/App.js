a2ceac20d4ab6b3c367ae656fc4d2754c063f540
Diego-1D <diego1d_fernandes@outlook.com>
2022-06-05 17:02:18 -0300

    Importações dos componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import Layout from "./components/Layout";
+import React from "react";
+import { BrowserRouter} from "react-router-dom";
+import {Container, Wrapper} from "./globalStyles"
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Routes from "./routes"
 
 function App() {
   return (
-    <>
-        <Layout/>
-    </>
+    <BrowserRouter>
+       <Container>
+        <Sidebar/>
+                <Wrapper>
+                  <Navbar/>
+                    <Header />
+                    <Routes />
+                </Wrapper>
+            </Container>
+    </BrowserRouter>
   );
 }
 
