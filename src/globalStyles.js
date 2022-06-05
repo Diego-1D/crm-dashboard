82f2668dcc37add9915db203614d96265cfc073c
Diego-1D <diego1d_fernandes@outlook.com>
2022-06-05 17:04:23 -0300

    Criação do estilo global

---
+++ src/globalStyles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+`;
+
+export const Wrapper = styled.div`
+    width: 100%;
+    min-height: 100vh;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: #D9DCFA;
+`;
